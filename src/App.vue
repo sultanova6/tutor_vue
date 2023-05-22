2954ea2d33d896833db11a201bd7799addecd59a
unknown <sultanova.6@mail.ru>
2023-05-22 17:41:02 +0300

    1

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
-
+  <div id="app">
+      <p>Hello</p>
+      <v-main-wrapper></v-main-wrapper>
+  </div>
 </template>
 
 <script>
-
+  import vMainWrapper from './components/v-main-wrapper'
 
 export default {
   name: 'App',
   components: {
-
+    vMainWrapper
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Nunito, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="v-main-wrapper">
+        <p>{{title}}</p>
+        <v-catalog />
+        <v-cart />
+    </div>
+</template>
+
+<script>
+ import vCatalog from './v-catalog'
+ import vCart from './v-cart'
+
+ export default {
+    name: 'v-main-wrapper',
+    components: {
+        vCatalog,
+        vCart
+    },
+    props: {},
+    data() {
+        return{
+            title: 'Main wrapper'
+        }
+    },
+    computed: {}, //вычисления
+    methods: {}, //анимация, то, что отлавливает действия пользователя
+    watch: {}, //следить за изменением элементов
+    mounted() {
+      console.log('Hello i am alive')
+    } //отрабатывает, когда все загрузитс
+ }
+</script>
+
+<style scoped>
+ .v-main-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-width: 900px;
+    margin: 0auto;
+    }
+</style>
